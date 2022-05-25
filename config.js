194e79bb6462201709c3cf9c0fcdbeee9c0c8334
kocotendung <106231026+kocotendung@users.noreply.github.com>
2022-05-25 16:28:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    titleWeb: "Thế giới của anhhh.",
-    introTitle: 'Việt Hà.',
+    titleWeb: "Thế giới của anhhh",
+    introTitle: 'Việt Hà',
     introDesc: `thật ra mình người mình thích chính là cậu đó<333.`,
     btnIntro: '^^HiHi^^.',
-    title: 'cậu có thích tớ không, cậu phải trả lời thật lòng đó nhaaa 🥰.',
-    desc: 'cậu không trả lời hay thoát ra coi như là cậu đồng ý nhaa<3 ',
+    title: 'cậu có thích tớ không, cậu phải trả lời thật lòng đó nhaaa 🥰',
+    desc: 'cậu không trả lời hay thoát ra coi như là cậu đồng ý nhaa<3',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'tại sao cậu lại thích tớ vậy ',
@@ -14,3 +14,4 @@ const CONFIG = {
     messDesc: 'vậy cậu làm người iu tớ nhé <3333.',
     btnAccept: 'Okiiiii lun <33',
     messLink: 'https://www.facebook.com/hoanghai.pham.16940599'
+}
